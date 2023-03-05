60c4be565bf2cd78a0fc7cc2eb46fa6ae480e188
Siddharth Paneri <dpaneri3@gmail.com>
2023-03-05 20:17:04 +0530

    new

--- loginpage.js
+++ loginpage.js
@@ -9,3 +9,9 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+const scrol1=document.querySelector("#navBar")
+    console.log(window.scrollY)
+    window.addEventEventListener("scroll",()=>{
+        if(window.scrollY >=10) scrol1.classList.add("navBar")
+        else scrol1.classList.remove("navBar")
+    })
--- student.js
+++ student.js
@@ -1,9 +1,8 @@
-const open1=document.querySelector("#openNav")
-const close1=document.querySelector("#closeNav")
+const open1=document.querySelector(".openNav")
+const close1=document.querySelector(".closeNav")
 open1.addEventListener("click",()=>{
     document.querySelector(".leftMenu").style.width="250px";
 })
 close1.addEventListener("click",()=>{
     document.querySelector(".leftMenu").style.width="0";
 })
-
