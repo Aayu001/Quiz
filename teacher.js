a0a1be03a4a4d9d11a9fa4751db2ffbf4cf9e7de
Siddharth Paneri <dpaneri3@gmail.com>
2023-03-11 20:09:25 +0530

    new

--- teacher.js
+++ teacher.js
@@ -1,3 +1,17 @@
+const open1=document.querySelector(".openNav")
+const close1=document.querySelector(".closeNav")
+open1.addEventListener("click",()=>{
+    document.querySelector(".leftMenu").style.display="block";
+    document.querySelector(".leftMenu").style.width="250px";
+    open1.classList.toggle("hide")
+    close1.classList.toggle("show")
+})
+close1.addEventListener("click",()=>{
+    document.querySelector(".leftMenu").style.display="none";
+    document.querySelector(".leftMenu").style.width="0";
+    open1.classList.toggle("hide")
+    close1.classList.toggle("show")
+})
 const addBtn=document.querySelector("#createClass")
 const addClass=document.querySelector(".modal")
 const cancel=document.querySelector("#classCancel")
