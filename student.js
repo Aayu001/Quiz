aa2033e98d35fc5c716f485b09a56e0e63ce6da6
Siddharth Paneri <dpaneri3@gmail.com>
2023-03-11 14:51:42 +0530

    new

--- student.js
+++ student.js
@@ -1,11 +1,13 @@
 const open1=document.querySelector(".openNav")
 const close1=document.querySelector(".closeNav")
 open1.addEventListener("click",()=>{
+    document.querySelector(".leftMenu").style.display="block";
     document.querySelector(".leftMenu").style.width="250px";
     open1.classList.toggle("hide")
     close1.classList.toggle("show")
 })
 close1.addEventListener("click",()=>{
+    document.querySelector(".leftMenu").style.display="none";
     document.querySelector(".leftMenu").style.width="0";
     open1.classList.toggle("hide")
     close1.classList.toggle("show")
