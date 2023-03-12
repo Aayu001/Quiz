2ca85c262a7aee3c9cc3d7815d550311a6483b6f
Siddharth Paneri <dpaneri3@gmail.com>
2023-03-12 19:58:26 +0530

    many changes

--- student.js
+++ student.js
@@ -1,17 +1,22 @@
+//for side bar
+
 const open1=document.querySelector(".openNav")
 const close1=document.querySelector(".closeNav")
+const main=document.querySelector("#mainContent")
+const menu=document.querySelector(".leftMenu")
 open1.addEventListener("click",()=>{
-    document.querySelector(".leftMenu").style.display="block";
     document.querySelector(".leftMenu").style.width="250px";
     open1.classList.toggle("hide")
     close1.classList.toggle("show")
+    main.classList.add("active")
 })
 close1.addEventListener("click",()=>{
-    document.querySelector(".leftMenu").style.display="none";
     document.querySelector(".leftMenu").style.width="0";
     open1.classList.toggle("hide")
     close1.classList.toggle("show")
+    main.classList.remove("active")
 })
+//for adding class
 const addBtns=document.querySelector("#createClass")
         const addClasses=document.querySelector(".joinClass")
         const canc=document.querySelector("#cancel")
--- teacher.js
+++ teacher.js
@@ -1,21 +1,28 @@
+//for side menu
+
 const open1=document.querySelector(".openNav")
 const close1=document.querySelector(".closeNav")
+const main=document.querySelector(".mainContent")
+const menu=document.querySelector(".leftMenu")
 open1.addEventListener("click",()=>{
-    document.querySelector(".leftMenu").style.display="block";
     document.querySelector(".leftMenu").style.width="250px";
     open1.classList.toggle("hide")
     close1.classList.toggle("show")
+    main.classList.add("active")
 })
 close1.addEventListener("click",()=>{
-    document.querySelector(".leftMenu").style.display="none";
     document.querySelector(".leftMenu").style.width="0";
     open1.classList.toggle("hide")
     close1.classList.toggle("show")
+    main.classList.remove("active")
 })
+
+//for adding class
 const addBtn=document.querySelector("#createClass")
 const addClass=document.querySelector(".modal")
 const cancel=document.querySelector("#classCancel")
 const form=document.querySelector("#class")
+
 form.addEventListener("click",(event)=>{
     event.preventDefault();
 })
