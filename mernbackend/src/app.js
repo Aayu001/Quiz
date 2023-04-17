4a3772a8f1f0e2187eb7aa0f4c9e82f8a015f052
Aayu001 <apaayush001@gmail.com>
2023-04-17 17:29:08 +0530

    backend

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+require("./db/conn");
+
 const port = process.env.PORT || 3000;
+
+//const static_path = path.join(__dirname)
+
+
+//app.use(express.static())
 
 app.get("/", (req, res) => {
     res.send("hello from aayush")
